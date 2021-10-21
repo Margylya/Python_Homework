f456c76caa98ac8b74d4b35432f523785fadfbf2
Margylya <62039430+Margylya@users.noreply.github.com>
2021-10-21 13:07:14 +0300

    Add files via upload

---
+++ 3 урок 2 задание.py
@@ -0,0 +1,3 @@
+def Database(**kwargs):
+    return kwargs
+print(Database(name = input("Введите ваше имя: "), Surname = input("Введите вашу фамилию: "), yearofbirth = input("Введите ваш год рождения: "), city = input("Введите ваш город: "), email = input("Ваш адрес электронной почты: "), tell = input("Введите ваш номер телефона: ") ))
